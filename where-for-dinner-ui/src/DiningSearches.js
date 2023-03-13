85b143b9d6074fd6cd0d7341cca7bcd78e119969
longevity <longevity@cluster.com>
2023-03-12 20:00:03 -0700

    1678676402

--- where-for-dinner-ui/src/DiningSearches.js
+++ where-for-dinner-ui/src/DiningSearches.js
@@ -16,7 +16,7 @@ function DiningSearches(props)
         <div name="diningSearches" id="diningSearches">
           <div className="bannerHeader"> 
             <div className="bannerCenter">
-            <label className="searchesHeader">Submitted Searches And Results1678669202</label>
+            <label className="searchesHeader">Submitted Searches And Results1678676402</label>
             </div>      	
           </div>           
           {searches}
